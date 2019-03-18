0d60f03825a30330f78aedc40ee9155e3feb39e5
Malick Diakite <malickdiakite496@gmail.com>
2019-03-18 10:28:56 +0530

    Code style update

--- Rolling_dice_game/rolling_dice_v0.js
+++ Rolling_dice_game/rolling_dice_v0.js
@@ -12,8 +12,7 @@ function playDice() {
     // Set the rules of the game
     alert("Welcome " + userName + ", to Dice rolling game.\nIn this game you choose one integer number between 1 to 6 and the computer roll the dice and check if you were lucky to pick the right number on the dice.\n And if so You are winner of the game.\n Beware that You are not allow to play more than 3 times");
    
-    do
-    {
+    do {
         // Set the limit for the game
         var limit = 3;
         // Set the limit of trials
@@ -24,7 +23,7 @@ function playDice() {
 
 
         // Prompt the user 3x if the provided number is not equal to the rand_int value
-        for(var count = 0; count < limit; count++)
+        for (var count = 0; count < limit; count++)
         {
             var userNumber = prompt("Enter one number between 1 and 6");
 
@@ -33,22 +32,21 @@ function playDice() {
 
                 
             // If the user enter a number greater than 6 Error alert will be dispalyed 
-            if(+userNumber > 6)
+            if (+userNumber > 6)
             {
                 alert("Error you should  choose an integer number only between 1 to 6.");
             }
             // If the user input is equal to  rand_int value display the message 
-            else if(+userNumber === rand_int)
-            {
-                alert("Congratulation " + userName + ", your number " +  +userNumber  + " correspond to the lucky number " + rand_int + ". You won the game!");
+            else if (+userNumber === rand_int) {
+                alert("Congratulation " + userName + ", your number " +  
+                +userNumber  + " correspond to the lucky number " + 
+                rand_int + ". You won the game!");
                 break;
             }
-            else
-            {
+            else {
                 // Decrease the limit of trials
                 trial--;
-                if(trial > 0)
-                {
+                if (trial > 0) {
                     alert("You loose the  lucky number was " + rand_int + ".  Try again" + userName + " you got " + trial + " chance to win!");
                 }
 
@@ -59,15 +57,14 @@ function playDice() {
 
 
         // Display the Game Over message if counter reach the limit of trials
-        if(counter === 3)
-        {
+        if (counter === 3) {
             alert("Game Over " + userName + " You loose.");
         }
 
         // Ask if the user desire to play 
         var ans = prompt("Wanna play again?\n Type(y) for yes (n) for no");
-    }
-    while(ans === 'Y' || ans === 'y'); //
+        // 
+    } while(ans === 'Y' || ans === 'y'); //
       
 }
   
